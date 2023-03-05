f8c794f014ad464ba4b11c3427ee615f1f69a439
Tai Karir <taikarir@Tais-MacBook-Air.local>
2023-03-05 17:01:58 -0500

    documentation and next step suggestions

--- src/main/cpp/subsystems/kinematicModel.cpp
+++ src/main/cpp/subsystems/kinematicModel.cpp
@@ -1,6 +1,7 @@
 #include <Subsystems/kinematicModel.h>
 #include <cmath>
 
+// calculates the position of a node based on the XYZ given by the april tags
 void kinematicModel::calculateRobotNode() {
     robotNodeX = robotAprilTagX - nodeAprilTagX;
     robotNodeY = robotAprilTagY - nodeAprilTagY;
@@ -11,14 +12,25 @@ void getAprilTagPosOfSelectedNode(int nodeRow) {
     
 };
 
+// finds the angles required for the robot to strafe/rotate towards the node
 void kinematicModel::calculateRobotAngles() {
     robotNodeDistance = cmath::hypot(robotNodeX, robotNodeY);
     robotAngleToNode = cmath::atan(robotNodeY / robotNodeX);
+
+    // for rotate-then-strafe method
+    // from here: robot_rotate(robotAprilTagYaw);
+    //            robot_strafe(robotNodeX, 0);
+    //            robot_arm_extend(robotNodeY);
+
+    // for rotate-only method
     robotAngleToFaceNode = 90 + robotAprilTagYaw - robotAngleToNode;
+    // from here: robot_rotate(robotAngleToFaceNode);
+    //            robot_arm_extend(robotNodeDistance);
+
+    // for strafe-only method
     robotStrafeToNodeAngle = robotAngleToNode - robotAprilTagYaw;
     robotStrafeToFaceNodeX = robotNodeDistance * cmath::cos(StrafeToNodeAngle);
     robotStrafeToFaceNodeY = robotNodeDistance * cmath::sin(StrafeToNodeAngle);
-
+    // from here: robot_strafe(robotStrafeToFaceNodeX, 0);
+    //            robot_arm_extend(robotStrafeToFaceNodeY);
 };
-
-    
