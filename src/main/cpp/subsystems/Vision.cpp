857da82ffd01c3d2e2b34aa660281cd345ee6f2f
Jack Jin <79951112+Rvistix@users.noreply.github.com>
2023-03-13 20:32:09 -0400

    Builds now... still have to work on the deprycated

--- src/main/cpp/subsystems/Vision.cpp
+++ src/main/cpp/subsystems/Vision.cpp
@@ -27,13 +27,12 @@ void Vision::updatePipeline(){
     cube = camera.GetLatestResult();
 }
 
-auto Vision::getListOfTags(){
-
 std::span<const photonlib::PhotonTrackedTarget> Vision::getListOfTags(){
 
     return tag.GetTargets();
 
 }
+   
 
 photonlib::PhotonPipelineResult Vision::getPipeline(int num) {
 
@@ -49,46 +48,4 @@ photonlib::PhotonPipelineResult Vision::getPipeline(int num) {
         default: 
             return tag;
     }
-#include <subsystems/Vision.h>
-
-Vision::Vision(){
-    camera.SetPipelineIndex(0);
-    tag = camera.GetLatestResult();
-    camera.SetPipelineIndex(1);
-    tape = camera.GetLatestResult();
-    camera.SetPipelineIndex(2);
-    cone = camera.GetLatestResult();
-    camera.SetPipelineIndex(3);
-    cube = camera.GetLatestResult();
-}
-
-void updatePipeline(){
-    camera.SetPipelineIndex(0);
-    tag = camera.GetLatestResult();
-    camera.SetPipelineIndex(1);
-    tape = camera.GetLatestResult();
-    camera.SetPipelineIndex(2);
-    cone = camera.GetLatestResult();
-    camera.SetPipelineIndex(3);
-    cube = camera.GetLatestResult();
-}
-
-photonlib::PhotonPipelineResult getPipeline(std::string name) {
-
-    switch(name) {
-        case "tag":
-            return tag;
-        case "tape":
-            return tape;
-        case "cube":
-            return cube;
-        case "cone":
-            return cone;
-        default: 
-            return
-    }
-
-}
-
-
 }
